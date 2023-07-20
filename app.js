fc127e15ed1e6a0c5d16e47c04840324ce6bcb7a
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-07-20 17:09:51 +0200

    Add switching

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const handSeconds = document.getElementById('hand-seconds');
-const handMinutes = document.getElementById('hand-minutes')
-const handHours = document.getElementById('hand-hours')
-const now = new Date()
+const handMinutes = document.getElementById('hand-minutes');
+const handHours = document.getElementById('hand-hours');
+
+const digitalSeconds = document.getElementById('digital-seconds');
+const digitalMinutes = document.getElementById('digital-minutes');
+const digitalHours = document.getElementById('digital-hours');
+
+const clockAnalog = document.getElementById('clock-analog');
+const clockDigital = document.getElementById('clock-digital');
+
+const clockSwitchButton = document.getElementById('clock-switch');
 
 const updateClockHands = () => {
+    const now = new Date();
     const seconds = now.getSeconds();
     const minutes = now.getMinutes();
     const hours = now.getHours();
@@ -11,8 +20,27 @@ const updateClockHands = () => {
     const minutesRotation = `rotate(${minutes * 6 + seconds / 10}deg)`;
     const hoursRotation = `rotate(${(hours % 12) * 30 + minutes / 2}deg)`;
 
-    handMinutes.style.transform = `${minutesRotation}`;
-    handHours.style.transform = `${hoursRotation}`;
+    handSeconds.style.transform = `rotate(${seconds * 6}deg)`;
+    handMinutes.style.transform = minutesRotation;
+    handHours.style.transform = hoursRotation;
+}
+
+const updateDigitalTime = () => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    digitalSeconds.innerText = `${seconds}`;
+    digitalMinutes.innerText = `${minutes}`;
+    digitalHours.innerText = `${hours}`;
+}
+
+const clockSwitch = () => {
+    clockAnalog.classList.toggle("clock__main--isDisabled");
+    clockDigital.classList.toggle("clock__main--isDisabled");
 }
 
 setInterval(updateClockHands, 1000);
+setInterval(updateDigitalTime, 1000);
+clockSwitchButton.addEventListener('click', clockSwitch);
